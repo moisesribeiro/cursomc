c3ca0ecd2bbb4bf13aff1bb991d7757197b34e42
Moisés Ribeiro <moises.rsjr@gmail.com>
2019-06-19 15:16:55 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/moisesribeiro/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/moisesribeiro/cursomc/resources/CategoriaResource.java
@@ -16,16 +16,15 @@ import com.moisesribeiro.cursomc.services.CategoriaService;
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
+
 	@Autowired
 	private CategoriaService service;
 
-	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
-		
-		
+
 	}
 
 }
--- src/main/java/com/moisesribeiro/cursomc/services/CategoriaService.java
+++ src/main/java/com/moisesribeiro/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.moisesribeiro.cursomc.domain.Categoria;
 import com.moisesribeiro.cursomc.repositories.CategoriaRepository;
+import com.moisesribeiro.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,6 +15,10 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Categoria obj = repo.findOne(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo: " + Categoria.class.getName());
+		}
 		return obj;
 	}
 
