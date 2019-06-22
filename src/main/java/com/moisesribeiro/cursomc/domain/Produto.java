0f8e11e4cc95de519ee1e0dbe909bec3aab7c28f
Moisés Ribeiro <moises.rsjr@gmail.com>
2019-06-21 22:20:44 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/moisesribeiro/cursomc/domain/Produto.java
+++ src/main/java/com/moisesribeiro/cursomc/domain/Produto.java
@@ -37,7 +37,7 @@ public class Produto implements Serializable {
 		return lista;
 	}
 
-	@JsonBackReference
+	@JsonIgnore
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private List<Categoria> categorias = new ArrayList<>();
